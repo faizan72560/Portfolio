82745bc084232de89c8aaad11e8eee20eef02f76
faizan72560 <faizanahmad72560@gmail.com>
2024-09-07 17:16:24 +0530

    poprtfolio done

---
+++ src/content_option.js
@@ -0,0 +1,164 @@
+const logotext = "Md Faizan Ahmad";
+const meta = {
+    title: "Md Faizan Ahmad",
+    // description: "I’m Md Faizan Ahmad Doe data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Md Faizan Ahmad",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "I am a dedicated software developer with a passion for building responsive, user-friendly web applications. With a strong foundation in React, Node.js, MongoDB, HTML, and CSS, I have successfully delivered several projects that emphasize clean code, scalability, and performance. My focus is always on creating solutions that meet client needs while staying adaptable in a fast-evolving tech landscape.",
+    your_img_url: "https://i.ibb.co/GFrs6CX/453629917-472811272371743-8709863391765290229-n-1.jpg",
+};
+
+const dataabout = {
+    title: "A Bit About Myself",
+    aboutme: "I am a dedicated software developer with a passion for building responsive, user-friendly web applications. With a strong foundation in React, Node.js, MongoDB, HTML, and CSS, I have successfully delivered several projects that emphasize clean code, scalability, and performance. My focus is always on creating solutions that meet client needs while staying adaptable in a fast-evolving tech landscape.",
+};
+const worktimeline = [{
+        jobtitle: "Redgates It Solution",
+        where: "Remote",
+        date: "2022",
+    },
+    {
+        jobtitle: "Kreed Sports Tech",
+        where: "Remote",
+        date: "2024",
+    },
+    
+];
+
+const skills = [{
+        name: "html",
+        value: 90,
+    },
+    {
+        name: "css",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 70,
+    },
+    {
+        name: "Nodejs",
+        value: 50,
+    },
+    {
+        name: "Mongodb",
+        value: 50,
+    },
+];
+
+const services = [{
+        title: "Website Development",
+        description: "I Have developed many website in the past 3 years and worked many different project.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Also worked on mobile application application",
+    },
+    
+];
+
+const dataportfolio = [{
+        img: "https://i.ibb.co/DKKgWsq/Screenshot-79.png",
+        description: "Cvmaker",
+        link: "https://cvmaker.me/",
+    },
+    {
+        img: "https://i.ibb.co/z4BD88g/Screenshot-80.png",
+        description: "Mloflo",
+        link: "https://mloflo.com/",
+    },
+    {
+        img: "https://i.ibb.co/MchkBjK/Screenshot-88.png",
+        description: "Amartal Cooling System.",
+        link: "https://play.google.com/store/apps/details?id=com.evdt.amartalgroup&pcampaignid=web_share",
+    },
+    {
+        img: "https://i.ibb.co/1MkBcXJ/Screenshot-90.png",
+        description: "Redgates",
+        link: "https://redgates.in/",
+    },
+    {
+        img: "https://i.ibb.co/R3FjmJg/Screenshot-91.png",
+        description: "Mindtrot",
+        link: "https://play.google.com/store/apps/details?id=com.mindtrot.patient&pcampaignid=web_share",
+    },
+    {
+        img: "https://i.ibb.co/f0CFp3X/Screenshot-92.png",
+        description: "Kreed",
+        link: "https://play.google.com/store/apps/details?id=com.kreed&pcampaignid=web_share",
+    },
+
+    // {
+    //     img: "https://picsum.photos/400/600/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/550/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "faizanahmad724560@gmail.com",
+    YOUR_FONE: "7256013760",
+    description: "I am a software developer with expertise in React, Node.js, MongoDB, HTML, and CSS. I specialize in building dynamic web applications and have a strong focus on clean, maintainable code and efficient problem-solving.",
+
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/faizan72560",
+    facebook: "https://www.facebook.com/profile.php?id=100089286232545",
+    linkedin: "https://www.linkedin.com/in/faizan-ahmad-32a9201bb/",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
